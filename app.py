efdfa8351023bf33e67e43698e63271bc42ddf38
Vaishnavi <vaish@vaishnavi.com>
2026-03-27 17:19:53 +0530

    message

---
+++ app.py
@@ -0,0 +1,128 @@
+from flask import Flask, render_template, request, redirect, session
+import sqlite3
+from model import predict
+
+app = Flask(__name__)
+app.secret_key = "secret123"
+
+def connect():
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        password TEXT,
+        role TEXT
+    )
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS records(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        text TEXT,
+        result TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+connect()
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = sqlite3.connect("database.db")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users WHERE username=? AND password=?",
+                    (username, password))
+        user = cur.fetchone()
+        conn.close()
+
+        if user:
+            session['user'] = username
+            session['role'] = user[3]
+
+            if user[3] == "admin":
+                return redirect('/admin')
+            else:
+                return redirect('/dashboard')
+        else:
+            return "Invalid Login ❌"
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        role = request.form['role']
+
+        conn = sqlite3.connect("database.db")
+        cur = conn.cursor()
+        cur.execute("INSERT INTO users(username,password,role) VALUES(?,?,?)",
+                    (username, password, role))
+        conn.commit()
+        conn.close()
+
+        return redirect('/')
+
+    return render_template('register.html')
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    if 'user' not in session:
+        return redirect('/')
+
+    result = None
+
+    if request.method == 'POST':
+        text = request.form['text']
+        result = predict(text)
+
+        conn = sqlite3.connect("database.db")
+        cur = conn.cursor()
+        cur.execute("INSERT INTO records(username,text,result) VALUES(?,?,?)",
+                    (session['user'], text, result))
+        conn.commit()
+        conn.close()
+
+    return render_template('dashboard.html', result=result)
+
+@app.route('/admin')
+def admin():
+    if 'role' not in session or session['role'] != 'admin':
+        return redirect('/')
+
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM records")
+    records = cur.fetchall()
+    conn.close()
+
+    return render_template('admin.html', records=records)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM records WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+
+    return redirect('/admin')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=10000)
---
+++ model.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import pickle
+import re
+import os
+file_path = os.path.join(os.path.dirname(__file__),"data.csv")
+data=pd.read_csv(file_path)
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+
+data = pd.read_csv("data.csv")
+
+X = data["text"]
+y = data["label"]
+
+vectorizer = TfidfVectorizer(stop_words='english')
+X_vector = vectorizer.fit_transform(X)
+
+model = MultinomialNB()
+model.fit(X_vector, y)
+
+pickle.dump(model, open("model.pkl", "wb"))
+pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'[^a-zA-Z]', ' ', text)
+    return text
+
+def predict(text):
+    model = pickle.load(open("model.pkl", "rb"))
+    vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+
+    text = clean_text(text)
+    text_vector = vectorizer.transform([text])
+    return model.predict(text_vector)[0]
